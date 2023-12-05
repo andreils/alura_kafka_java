022e4bba5e3384d1088f653eb189662680455e4e
andreils <andreiwz@gmail.com>
2023-12-05 00:26:55 -0300

    simplifica logica

--- src/main/java/com/alura/ecommerce/EmailService.java
+++ src/main/java/com/alura/ecommerce/EmailService.java
@@ -1,6 +1,7 @@
 package com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -11,36 +12,20 @@ import java.util.Properties;
 public class EmailService {
 
     public static void main(String[] args)  {
-        var consumer = new KafkaConsumer<>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
-        while (true){
-            System.out.println("Procurando");
-            var records = consumer.poll(Duration.ofMillis(100));
-            if(!records.isEmpty()){
-                System.out.println("Encontrou " + records.count());
-                for (var record : records){
-                    System.out.println("============================");
-                    System.out.println("Processing Email");
-                    System.out.println("KEY " + record.key());
-                    System.out.println("VALOR " + record.value());
-                    System.out.println("OFFSET " + record.offset());
-                    System.out.println("Email processed");
-                }
-            }
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        var emailService = new EmailService();
+        var kafkaService = new KafkaService(EmailService.class.getSimpleName()
+                ,"ECOMMERCE_SEND_EMAIL"
+                ,emailService::parser);
+        kafkaService.run();
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-        return properties;
+    private void parser(ConsumerRecord<String, String> record){
+        System.out.println("============================");
+        System.out.println("Processing Email");
+        System.out.println("KEY " + record.key());
+        System.out.println("VALOR " + record.value());
+        System.out.println("OFFSET " + record.offset());
+        System.out.println("Email processed");
     }
+
 }
--- src/main/java/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/com/alura/ecommerce/FraudDetectorService.java
@@ -1,6 +1,7 @@
 package com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -12,41 +13,22 @@ import java.util.Properties;
 public class FraudDetectorService {
 
     public static void main(String[] args)  {
-        var consumer = new KafkaConsumer<>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-        while (true){
-            var records = consumer.poll(Duration.ofMillis(100));
-            if(!records.isEmpty()){
-                System.out.println("Encontrou " + records.count());
-                var i = 1;
-                for (var record : records){
-                    System.out.println("============================");
-                    System.out.println("Processing new order, checking for fraud");
-                    System.out.println("KEY " + record.key());
-                    System.out.println("VALOR " + record.value());
-                    System.out.println("OFFSET " + record.offset());
-                    System.out.println("PARTITION " + record.partition());
-                    System.out.println("PROCESSASNDO " + i + " : " + records.count());
-                    System.out.println("Order processed");
-
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        }
+        var fraudDetectorService = new FraudDetectorService();
+        var kafkaService = new KafkaService(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudDetectorService::parser);
+        kafkaService.run();
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-
-        return properties;
+    private void parser(ConsumerRecord<String, String> record) {
+        System.out.println("============================");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println("KEY " + record.key());
+        System.out.println("VALOR " + record.value());
+        System.out.println("OFFSET " + record.offset());
+        System.out.println("PARTITION " + record.partition());
+//        System.out.println("PROCESSASNDO " + i + " : " + records.count());
+        System.out.println("Order processed");
     }
+
 }
